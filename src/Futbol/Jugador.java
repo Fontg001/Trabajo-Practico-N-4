f6642f2c61884c3d02f3fb6661fd79cd8fe8e1df
Fontg001 <fontg367@gmail.com>
2024-06-03 02:59:33 -0300

    First Commit

---
+++ src/Futbol/Jugador.java
@@ -0,0 +1,21 @@
+
+package Futbol;
+
+class Jugador extends Persona {
+    private String posicion;
+    private int dorsal;
+    private int golesAnotados;
+
+    public Jugador(String nombre, int edad, String posicion, int dorsal) {
+        super(nombre, edad);
+        this.posicion = posicion;
+        this.dorsal = dorsal;
+        this.golesAnotados = 0;
+    }
+
+    // Getters and setters for posicion, dorsal, and golesAnotados
+
+    String getPosicion() {
+        return posicion;
+    }
+}
